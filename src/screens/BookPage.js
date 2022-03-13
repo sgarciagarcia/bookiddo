784dc1176343d114c10de4672d5c63b2d2957165
Sara García <sargarciagarcia@gmail.com>
2022-03-13 14:12:41 +0100

    Add error message if there is no recommended books

--- src/screens/BookPage.js
+++ src/screens/BookPage.js
@@ -1,10 +1,21 @@
 
-import BookCard from './components/BookCard';
+import { useParams } from 'react-router';
+import { useContext } from 'react';
 
-export default BookPage = () => {
-    <>
-    <p>Hola, esto es la ficha de un libro</p>
-    <BookCard/>
-    </>
+import { FormContext } from '../FormContext'
+import BookCard from '../components/books/BookCard';
 
+const BookPage = () => {
+    const {booksData} = useContext(FormContext);
+    const {isbn} = useParams();
+    const thisBook = booksData.filter((book1) => book1.isbn === isbn)
+    
+    return(
+        <>
+        <p>Hola, esto es la ficha de un libro y aquí van sus datos</p>
+        <BookCard title={thisBook[0].title} cover={thisBook[0].cover} author={thisBook[0].author}/>
+        </>
+    )
+   
 }
+export default BookPage
