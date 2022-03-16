f9ad3d0fbb07fc020563f25b01e4f5e450e20eec
Sara García <sargarciagarcia@gmail.com>
2022-03-16 13:29:47 +0100

    Style LogOut button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,13 @@
-import { useContext } from 'react';
 
 import Recommendations from '../components/Recommendations';
 import Header from '../components/Header';
 import Menu from '../components/Menu';
-import { FormContext } from '../FormContext';
 
 
 const Home = () => {
-    const { handleLogOut } = useContext(FormContext);
 
     return (
     <>
-    <button type="button" onClick={handleLogOut}>Log out</button>
     <Header/>
     <Recommendations/>
     <Menu/>
